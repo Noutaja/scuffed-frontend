5fac67f9b652d3f087bda55f115c2e19eda8dcb2
Noutaja <jyri.jarvinen@hotmail.com>
2023-09-21 08:50:27 +0300

    General setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { ThemeProvider, createTheme } from "@mui/material";
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+import MainPage from "./pages/MainPage";
+import SingleProductPage from "./pages/SingleProductPage";
+import Header from "./components/Header";
+import UserPage from "./pages/UserPage";
+import CartPage from "./pages/CartPage";
+import Footer from "./components/Footer";
+import store from "./redux/store";
+
+function App() {
+	return (
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<HashRouter basename="/">
+					<Header />
+					<Routes>
+						<Route path="/" element={<MainPage />} />
+						<Route path="products/:productID" element={<SingleProductPage />} />
+						<Route path="users/:userID" element={<UserPage />} />
+						<Route path="cart" element={<CartPage />} />
+					</Routes>
+					<Footer />
+				</HashRouter>
+			</ThemeProvider>
+		</Provider>
+	);
 }
 
-export default App
+const theme = createTheme({
+	typography: {
+		fontSize: 12,
+	},
+});
+
+export default App;
