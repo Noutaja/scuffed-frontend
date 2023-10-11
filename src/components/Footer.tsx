c985977bf9fa9ba7610d65b305648b1887218cc2
Noutaja <jyri.jarvinen@hotmail.com>
2023-10-11 21:31:29 +0300

    Added proper footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Footer() {
 	return (
-		<Box component="footer" marginTop="auto">
-			<Typography>Footer</Typography>
+		<Box component="footer" marginTop="auto" display="flex" justifyContent="center" sx={{backgroundColor: "rgb(25, 118, 210)", color:"white"}}>
+			<Typography variant="h5" sx={{margin:"auto"}}>© 2023 Jyri Järvinen</Typography>
 		</Box>
 	);
 }
