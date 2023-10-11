f27ef5341a7db53f5714a276ff9e7c7e347aa665
Noutaja <jyri.jarvinen@hotmail.com>
2023-10-11 21:32:06 +0300

    changed copyright text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
 	return (
 		<Box component="footer" marginTop="auto" display="flex" justifyContent="center" sx={{backgroundColor: "rgb(25, 118, 210)", color:"white"}}>
-			<Typography variant="h5" sx={{margin:"auto"}}>© 2023 Jyri Järvinen</Typography>
+			<Typography variant="h5" sx={{margin:"auto"}}>© 2023 Noutaja</Typography>
 		</Box>
 	);
 }
